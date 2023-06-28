a6f5a0fa019643c2118b07ceb93343dae9aed97b
Jason Altekruse <altekrusejason@gmail.com>
2023-06-28 08:41:30 -0500

    adding built version of inlined web worker

--- dist/librarytest.es.js
+++ dist/librarytest.es.js
@@ -1,10 +1,35 @@
-const s = new Worker("./lib/worker.js");
-function t(e, o) {
+function a(e) {
+  return Math.sqrt(e) * 3;
+}
+window.sqrtImpl = a;
+var t = URL.createObjectURL(
+  new Blob(
+    [
+      "(",
+      function() {
+        onmessage = function(e) {
+          e.data.messageType === "squareRoot" && postMessage({
+            messageType: "squareRootResult",
+            //result: window.sqrtImpl(e.data.num),
+            result: Math.sqrt(e.data.num),
+            destId: e.data.destId
+          });
+        };
+      }.toString(),
+      ")()"
+    ],
+    { type: "application/javascript" }
+  )
+);
+delete window.worker;
+const s = new Worker(t);
+URL.revokeObjectURL(t);
+function r(e, o) {
   s.postMessage({ messageType: "squareRoot", num: o, destId: e });
 }
 s.onmessage = (e) => {
   console.log("Message received from worker", e), e.data.messageType === "squareRootResult" && (document.getElementById(e.data.destId).innerHTML = e.data.result);
 };
 export {
-  t as squareRoot
+  r as squareRoot
 };
